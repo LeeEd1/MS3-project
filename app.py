2eae75062e649928caa327bfbaf1d43ae515434d
LeeEd1 Edwards <edwardsla@live.co.uk>
2024-04-24 15:05:11 +0000

    Added template and route for add recipe and corresponding link

--- app.py
+++ app.py
@@ -112,6 +112,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_recipe")
+def add_recipe():
+    return render_template("add_recipe.html")
+
+
 # Runs flask app
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
